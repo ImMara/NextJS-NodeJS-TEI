fd600f5911656af4cff0faa2a65c8bd2fdc3000c
Mara <alaneraerts@live.be>
2021-09-01 22:47:46 +0200

    :green_circle: types menu

--- server/database/models/menu.model.js
+++ server/database/models/menu.model.js
@@ -2,9 +2,22 @@ const mongoose = require('mongoose');
 const schema = mongoose.Schema;
 
 const menuSchema = schema({
-    page_id:{ type: 'integer'},
-    title: { type: 'string'},
-    parent: { type: 'string' },
+    page_id:{
+        type:schema.Types.ObjectId,
+        ref:"page",
+        required: true
+    },
+    title: {
+        type: String,
+        minLength: [1,"title is too short"],
+        maxLength: [125,"title is too long"],
+        unique: true,
+        required: true
+    },
+    parent: {
+        type: schema.Types.ObjectId,
+        ref:"menu",
+    },
 })
 
 let Menu;
