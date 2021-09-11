651ac5ad26f367a7aeefb1fa488cd91f6549ebae
Mara <alaneraerts@live.be>
2021-09-11 08:05:53 +0200

    settings ui

--- pages/np-admin/params/index.js
+++ pages/np-admin/params/index.js
@@ -24,41 +24,41 @@ function Index(props) {
 
     return (
         <Navbar>
-            <h1>Params</h1>
-            <form action="">
-                <label htmlFor="">
-                    title
-                    <input type="text" onChange={handleChange} defaultValue={setting[0].title} name="title"/>
-                </label>
-                <label htmlFor="">
-                    email
-                    <input type="email" onChange={handleChange} defaultValue={setting[0].email} name="email"/>
-                </label>
-                <label htmlFor="">
-                    url
-                    <input type="text" onChange={handleChange} defaultValue={setting[0].url} name="url"/>
-                </label>
-                <label htmlFor="">
-                    role
-                    <select name="defaultRoles" defaultValue={setting[0].role}>
+            <h1>Settings</h1>
+            <hr/>
+            <form>
+
+                <div className="mb-3">
+                    <label htmlFor="title" className="form-label">title : </label>
+                    <input className="form-control" id="title" type="text" onChange={handleChange} defaultValue={setting[0].title} name="title"/>
+                </div>
+
+                <div className="mb-3">
+                    <label htmlFor="email" className="form-label">email</label>
+                    <input className="form-control" id="email" type="email" onChange={handleChange} defaultValue={setting[0].email} name="email"/>
+                </div>
+
+                <div className="mb-3">
+                    <label htmlFor="url" className="form-label">url</label>
+                    <input className="form-control" id="url" type="text" onChange={handleChange} defaultValue={setting[0].url} name="url"/>
+                </div>
+
+                <div className="mb-3">
+                    <label htmlFor="role" className="form-label">role</label>
+                    <select className="form-control" id='role' name="defaultRoles" defaultValue={setting[0].role}>
                         <option onChange={handleChange} value="role_admin">Admin</option>
                     </select>
-                </label>
-                <label htmlFor="">
-                    slogan
-                    <textarea onChange={handleChange} name="slogan" defaultValue={setting[0].slogan} cols="30" rows="10"/>
-                </label>
-                <label htmlFor="">
-                    comments
-                    {
-                        setting[0].comments ?
-                        (
-                            <input onChange={handleChange} checked={true} type="checkbox"  name="comments"/>
-                        ) : (
-                            <input onChange={handleChange} checked={false}  type="checkbox" name="comments"/>
-                        )
-                    }
-                </label>
+                </div>
+
+                <div className="mb-3">
+                    <label htmlFor="slogan" className="form-label">slogan</label>
+                    <textarea className="form-control" id="slogan" onChange={handleChange} name="slogan" defaultValue={setting[0].slogan} cols="30" rows="10"/>
+                </div>
+
+                <div className="mb-3">
+                    <input id="comments" className="form-check-input" onChange={handleChange} type="checkbox" name="comments"/>
+                    <label htmlFor="comments" className="form-check-label"> comments </label>
+                </div>
                 <a className="btn btn-primary" onClick={handleSubmit}>Submit</a>
             </form>
         </Navbar>
