ec1a244d63480af6930732b66718e479de921b1d
Mara <alaneraerts@live.be>
2021-09-15 08:48:22 +0200

    udpdate

--- pages/np-admin/blog/index.js
+++ pages/np-admin/blog/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Navbar from "../../../components/admin/navbar/Navbar";
 import Table from "../../../components/bootstrap-5/table/Table";
 import {getPosts} from "../../../server/queries/post.queries";
+import Modal from "../../../components/bootstrap-5/modal/Modal";
 
 function Index(props) {
     return (
@@ -14,23 +15,12 @@ function Index(props) {
             </button>
 
             {/* Modal */}
-            <div className="modal fade" id="exampleModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                <div className="modal-dialog">
-                    <div className="modal-content">
-                        <div className="modal-header">
-                            <h5 className="modal-title" id="exampleModalLabel">Modal title</h5>
-                            <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"/>
-                        </div>
-                        <div className="modal-body">
-                            ...
-                        </div>
-                        <div className="modal-footer">
-                            <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                            <button type="button" className="btn btn-primary">Save changes</button>
-                        </div>
-                    </div>
-                </div>
-            </div>
+            <Modal
+                target={"exampleModal"}
+                label={"exampleModalLabel"}
+                title={"Modal title"}
+            >...</Modal>
+
             <Table data={[{
                 name:"Jean",
                 lastName:"Jacod",
