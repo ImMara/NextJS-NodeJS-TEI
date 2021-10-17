f2a3592278647a8a0d6a5eece5ca97f81d6fbd4b
Mara <alaneraerts@live.be>
2021-10-17 18:18:29 +0200

    18-18

--- templates/components/Posts/Posts.jsx
+++ templates/components/Posts/Posts.jsx
@@ -39,7 +39,7 @@ function Posts(props) {
                                             <a href="#" className={"position-absolute bottom-0 start-0 link-dark ms-3 mb-3 badge bg-primary me-2 small fw-bold"}><i className="fas fa-circle me-2 small fw-bold"/>{props.category ? props.category.title : "no category"}</a>
                                         </div>
                                         <h4 className={"mt-1"}>{post.title}</h4>
-                                        <p>{post.short_description}</p>
+                                        <p className={"w-100"}>{post.short_description}</p>
                                         <div className={"d-flex align-items-center"}>
                                             <h5 className="me-3">{ post.author !== null ? post.author.username : "anonymous"}</h5>
                                             <span className="small">{post.date}</span>
