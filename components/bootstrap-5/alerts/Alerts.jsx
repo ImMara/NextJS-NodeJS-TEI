521ef3bbd02ef6a983961e802c01bed5a881d974
Mara <alaneraerts@live.be>
2021-10-18 22:41:34 +0200

    responsiv carousel

--- components/bootstrap-5/alerts/Alerts.jsx
+++ components/bootstrap-5/alerts/Alerts.jsx
@@ -12,7 +12,7 @@ function Alerts(props) {
     }
 
     return (
-        <div className={`alert alert-${alertTypes()}`} role="alert">
+        <div className={`alert alert-${alertTypes()} alert-dismissible fade show`} role="alert">
             {props.message}
             <button type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Close"/>
         </div>
