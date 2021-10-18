3d53abf220cf83f437ad7ed1ab23b996b90f0d53
Mara <alaneraerts@live.be>
2021-10-18 14:54:40 +0200

    responsiv carousel

--- templates/components/Posts/Posts.jsx
+++ templates/components/Posts/Posts.jsx
@@ -1,11 +1,11 @@
-import React, {useEffect, useState} from 'react';
-import axios from "axios";
+import React, {useState} from 'react';
 import Link from 'next/link';
 import Pagination from "../../../components/shared/Pagination/Pagination";
+
 function Posts(props) {
 
-    const [posts,setPosts]= useState(props.posts);
-    const [loading,setLoading] = useState(true);
+    const [posts, setPosts] = useState(props.posts);
+    const [loading, setLoading] = useState(true);
 
     //pagination
     const [currentPage, setCurrentPage] = useState(1);
@@ -24,57 +24,61 @@ function Posts(props) {
 
     return (
         <div className="col-lg-9 mb-5">
-                        <div className="mb-5">
-                            <h2 className={"m-0"}><i className="bi bi-hourglass-top me-2"/>L'actu en continu </h2>
-                            <p>le site dédié aux jeux vidéo sur Consoles et PC. Découvrez l'actualité des jeux vidéo .</p>
-                        </div>
+            <div className="mb-5">
+                <h2 className={"m-0"}><i className="bi bi-hourglass-top me-2"/>L'actu en continu </h2>
+                <p>le site dédié aux jeux vidéo sur Consoles et PC. Découvrez l'actualité des jeux vidéo .</p>
+            </div>
 
-                        <div className={"mb-3"}>
-                            {itemsPerPage < posts.length &&
-                            (
-                                <Pagination
-                                    currentPage={currentPage}
-                                    itemsPerPage={itemsPerPage}
-                                    length={posts.length}
-                                    onPageChanged={handlePageChange}
-                                />
-                            )
-                            }
-                        </div>
+            <div className={"d-flex flex-column justify-content-between"}>
 
-                        <div className="row position-relative" style={{minHeight:'850px'}}>
-                        {
-                            paginatedData.map((post,index) => (
-                                <Link href={"/post/" + post._id}>
-                                    <div className="col-md-6 mb-3" style={{cursor: "pointer"}}  key={index}>
-                                        <div className="position-relative">
-                                            <a href="#" className={"position-absolute p-2 bottom-0 start-0 link-light ms-3 mb-3 badge bg-primary me-2 small fw-bold"}><i className="fas fa-circle me-2 small fw-bold"/>{post.category ? post.category.title : "no category"}</a>
-                                            <img className={"rounded"} src={"/images/blogs/resized/"+post.image} style={{height:"200px",width:"100%",objectFit:"cover"}} alt=""/>
-                                        </div>
-                                        <h4 className={"mt-1"}>{post.title}</h4>
-                                        <p className={"w-100"}>{post.short_description}</p>
-                                        <div className={"d-flex align-items-center"}>
-                                            <h5 className="me-3">{ post.author !== null ? post.author.username : "anonymous"}</h5>
-                                            <span className="small">{post.date.substring(0, 10)}</span>
-                                        </div>
+                <div className={"mb-3"}>
+                    {itemsPerPage < posts.length &&
+                    (
+                        <Pagination
+                            currentPage={currentPage}
+                            itemsPerPage={itemsPerPage}
+                            length={posts.length}
+                            onPageChanged={handlePageChange}
+                        />
+                    )
+                    }
+                </div>
+
+                <div className="row" style={{minHeight: '850px'}}>
+                    {
+                        paginatedData.map((post, index) => (
+                            <Link href={"/post/" + post._id}>
+                                <div className="col-md-6 mb-3" style={{cursor: "pointer"}} key={index}>
+                                    <div className="position-relative">
+                                        <a href="#"
+                                           className={"position-absolute p-2 bottom-0 start-0 link-light ms-3 mb-3 badge bg-primary me-2 small fw-bold"}><i
+                                            className="fas fa-circle me-2 small fw-bold"/>{post.category ? post.category.title : "no category"}
+                                        </a>
+                                        <img className={"rounded"} src={"/images/blogs/resized/" + post.image}
+                                             style={{height: "200px", width: "100%", objectFit: "cover"}} alt=""/>
                                     </div>
-                                </Link>
-                            ))
-                        }
-                            <div>
-                                {itemsPerPage < posts.length &&
-                                (
-                                    <Pagination
-                                        currentPage={currentPage}
-                                        itemsPerPage={itemsPerPage}
-                                        length={posts.length}
-                                        onPageChanged={handlePageChange}
-                                    />
-                                )
-                                }
-                            </div>
-
-                        </div>
+                                    <h4 className={"mt-1"}>{post.title}</h4>
+                                    <p className={"w-100"}>{post.short_description}</p>
+                                    <div className={"d-flex align-items-center"}>
+                                        <h5 className="me-3">{post.author !== null ? post.author.username : "anonymous"}</h5>
+                                        <span className="small">{post.date.substring(0, 10)}</span>
+                                    </div>
+                                </div>
+                            </Link>
+                        ))
+                    }
+                </div>
+                <div>
+                    {itemsPerPage < posts.length && (
+                        <Pagination
+                            currentPage={currentPage}
+                            itemsPerPage={itemsPerPage}
+                            length={posts.length}
+                            onPageChanged={handlePageChange}
+                        />
+                    )}
+                </div>
+            </div>
         </div>
     );
 }
